319e1b96e0270970eaedf502e414fae2c082db22
Astar <bahouidi99@gmail.com>
2023-07-24 19:54:37 +0100

    Retreive values from form event object

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,25 +1,22 @@
-import React, {useRef} from 'react';
-const BasicForm = (props) => {
-	const nameInputRef = useRef();
-	const passwdInputRef = useRef();
-
-	const handleSubmit = (e) => {
-		const name = nameInputRef.current.value; 
-		const passwd = passwdInputRef.current.value; 
-		e.preventDefault();
-		alert(name + ' - ' + passwd);
-	} 
+import React from "react";
+const BasicForm = () => {
+  const handleSubmit = (e) => {
+    const name = e.target.elements.email.value;
+    const passwd = e.target.elements.passwd.value;
+    e.preventDefault();
+    alert(name + " - " + passwd);
+  };
   return (
     <form onSubmit={handleSubmit}>
-      <div className='form-control'>
-        <label htmlFor='email'>E-Mail Address</label>
-        <input type='text' id='email' ref={nameInputRef} />
+      <div className="form-control">
+        <label htmlFor="email">E-Mail Address</label>
+        <input type="text" id="email" />
       </div>
-      <div className='form-control'>
-        <label htmlFor='password'>Your password</label>
-        <input type='password' id='passwd' ref={passwdInputRef}/>
+      <div className="form-control">
+        <label htmlFor="password">Your password</label>
+        <input type="password" id="passwd" />
       </div>
-      <div className='form-actions'>
+      <div className="form-actions">
         <button>Submit</button>
       </div>
     </form>
