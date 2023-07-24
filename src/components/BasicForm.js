3520f5fc3faa53d7d7ab6b7df139a86568910bdd
Astar <bahouidi99@gmail.com>
2023-07-24 19:46:47 +0100

    Retreive values with useRef

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,19 +1,23 @@
+import React, {useRef} from 'react';
 const BasicForm = (props) => {
+	const nameInputRef = useRef();
+	const passwdInputRef = useRef();
+
+	const handleSubmit = (e) => {
+		const name = nameInputRef.current.value; 
+		const passwd = passwdInputRef.current.value; 
+		e.preventDefault();
+		alert(name + ' - ' + passwd);
+	} 
   return (
-    <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
-        </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
-        </div>
+    <form onSubmit={handleSubmit}>
+      <div className='form-control'>
+        <label htmlFor='email'>E-Mail Address</label>
+        <input type='text' id='email' ref={nameInputRef} />
       </div>
       <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+        <label htmlFor='password'>Your password</label>
+        <input type='password' id='passwd' ref={passwdInputRef}/>
       </div>
       <div className='form-actions'>
         <button>Submit</button>
