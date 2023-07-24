2e0037a3cf183c3ce88bbc01d90b8957b15c56ee
Astar <bahouidi99@gmail.com>
2023-07-24 20:16:56 +0100

    Idiomatic controlled react form

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+
 const BasicForm = () => {
+  const [inputEmail, setInputEmail] = useState("");
+  const [inputPasswd, setInputPasswd] = useState("");
+
+  const handleEmailChange = (e) => {
+    setInputEmail(e.target.value);
+  };
+  const handlePasswdChange = (e) => {
+    setInputPasswd(e.target.value);
+  };
+
   const handleSubmit = (e) => {
-    const name = e.target.elements.email.value;
-    const passwd = e.target.elements.passwd.value;
     e.preventDefault();
-    alert(name + " - " + passwd);
+    const email = inputEmail.trim();
+    const passwd = inputPasswd.trim();
+    alert(email + " - " + passwd);
   };
   return (
     <form onSubmit={handleSubmit}>
       <div className="form-control">
         <label htmlFor="email">E-Mail Address</label>
-        <input type="text" id="email" />
+        <input
+          type="text"
+          id="email"
+          value={inputEmail}
+          onChange={handleEmailChange}
+        />
       </div>
       <div className="form-control">
         <label htmlFor="password">Your password</label>
-        <input type="password" id="passwd" />
+        <input
+          type="password"
+          id="passwd"
+          value={inputPasswd}
+          onChange={handlePasswdChange}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
